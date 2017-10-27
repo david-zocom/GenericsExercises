a2f7cab4568ad6e3f9a2d6c34d9e8b0c308a0e02
David Andersson <David Andersson>
2017-10-27 14:29:24 +0200

    delegates 5

--- GenericsExercises/Program.cs
+++ GenericsExercises/Program.cs
@@ -6,16 +6,17 @@ using System.Threading.Tasks;
 
 namespace GenericsExercises
 {
-	// https://github.com/lejonmanen/GenericsExercises
 	class Program
 	{
 		static void Main(string[] args)
 		{
-			Trio<string> trio = new Trio<string>();
+			/*Trio<string> trio = new Trio<string>();
 			trio.First = "första";
 			trio.Second = "andra";
 			Trio2<Vehicle, Car, Bike> trio2 = new Trio2<Vehicle, Car, Bike>();
-
+			*/
+			ListFunctions lf = new ListFunctions();
+			lf.Demo();
 		}
 	}
 
@@ -36,5 +37,48 @@ namespace GenericsExercises
 		public T1 First { get; set; }
 		public T2 Second { get; set; }
 		public T3 Third { get; set; }
+	}
+
+	/*5 Skapa en funktion som tar en List<T> och en Func<T, T, T> delegate som parametrar och returnerar ett värde av typen T. Du bestämmer själv vilken datatyp du vill använda i stället för T. Funktionen ska heta Reduce och den ska reducera listan till ett värde. Om listan till exempel består av strängar så är ett sätt att reducera den att konkatenera alla strängar i listan och returnera resultatet. Om listan består av tal så kan man reducera listan genom att räkna ut till exempel summan, max- eller min-värdet. Delegaten ska alltså vara funktionen som utför själva reduceringen.*/
+	public class ListFunctions
+	{
+		public void Demo()
+		{
+			List<int> list = new List<int>();
+			list.Add(5);
+			list.Add(10);
+			list.Add(1000);
+			int sum = Reduce(list, Sum);
+			sum = Reduce(list, (x, y) => x + y);
+		}
+
+		Func<string, string, string> f = (x, y) => x + y;
+		private static string Concat(string x, string y)
+		{
+			return x + y;
+		}
+		
+		private static int Sum(int x, int y)
+		{
+			return x + y;
+		}
+		private static int Max(int x, int y)
+		{
+			if (x > y) return x;
+			else return y;
+		}
+		private Func<string, string, string> ConcatDelegate = Concat;
+		//Func<T, T, T> Min
+
+		// Reduce == aggregate
+		public static T Reduce<T>(List<T> list, Func<T, T, T> reducer)
+		{
+			T soFar = list[0];
+			for (int i = 1; i < list.Count; i++)
+			{
+				soFar = reducer(soFar, list[i]);
+			}
+			return soFar;
+		}
 	}
 }
